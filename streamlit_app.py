88a9822c13adb3a847fcdc963bd75f00dc591009
sarting118 <79422138+sarting118@users.noreply.github.com>
2022-08-22 12:14:11 +0200

    Update streamlit_app.py
    added subheader and first menu option

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
+# add a title
 streamlit.title('My Parents new healthy diner')
+# add a header
+streamlit.header('Breakfast Menu')
+# add menu options
+streamlit.text('Omega 3 & Blueberry Oatmeal')
