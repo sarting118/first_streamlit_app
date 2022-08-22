a7e3c41d55297e939eda22b5b7692b1f9c370d29
sarting118 <79422138+sarting118@users.noreply.github.com>
2022-08-22 12:35:25 +0200

    Update streamlit_app.py
    modified menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,16 @@ streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Faves')
 # if using two .text options I get spaces between lines
 # add menu options
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 # add menu options
 # streamlit.text('Kale, Spinach & Rocket Smoothie  \nHard-Boiled Free-Range Egg')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 # alternative to the above - no spaces between lines
 # streamlit.text('Omega 3 & Blueberry Oatmeal  \nKale, Spinach & Rocket Smoothie  \nHard-Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍇 Build your own fruit smoothie 🍌')
