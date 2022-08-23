e9ff9e02d48d0f5a78b81746d7a609003c916248
sarting118 <79422138+sarting118@users.noreply.github.com>
2022-08-23 15:41:08 +0200

    Update streamlit_app.py
    added user input to select the fruit they wanna get information on

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,9 +37,11 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 # API request to fruityvice.com
-fruityvice_response = re.get('https://fruityvice.com/api/fruit/watermelon')
+fruityvice_response = re.get('https://fruityvice.com/api/fruit/' + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 # normalise the json version of the response
