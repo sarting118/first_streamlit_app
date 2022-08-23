e4868545fc11eca319aff710043fa98f633831c0
sarting118 <79422138+sarting118@users.noreply.github.com>
2022-08-23 17:21:48 +0200

    Update streamlit_app.py
    displaying results in table instead of single row

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,4 +59,6 @@ my_cur.execute('select * from fruit_load_list')
 my_data_row = my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
 streamlit.header('The fruit load list contains:')
-streamlit.text(my_data_row)
+# display results in dataframe instead of lsit
+# streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
