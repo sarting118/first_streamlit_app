59d0b2ced812ca2714b2633d46442d47782d7c83
sarting118 <79422138+sarting118@users.noreply.github.com>
2022-08-23 18:14:08 +0200

    Update streamlit_app.py
    removed hard-coded 'from streamlit'

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,6 +79,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   # display results in dataframe instead of lsit
   # streamlit.text(my_data_row)
   streamlit.dataframe(my_data_rows)
@@ -89,7 +90,7 @@ if streamlit.button('Get Fruit Load List'):
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     # add rows in snowflake fruit list
-    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
 
 # adding an entry box
@@ -97,5 +98,6 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
 
