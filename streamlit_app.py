0f3540aea1e397d2b9b7e35747a2b85aeddf031a
sarting118 <79422138+sarting118@users.noreply.github.com>
2022-08-23 17:44:42 +0200

    Update streamlit_app.py
    adding try-except for fruit advice section

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,17 +39,23 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    # API request to fruityvice.com
+  fruityvice_response = re.get('https://fruityvice.com/api/fruit/' + fruit_choice)
+  # streamlit.text(fruityvice_response.json())
+  # normalise the json version of the response
+  fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+  # output the dataframe to the screen as a table
+  streamlit.dataframe(fruityvice_normalised)
+#   streamlit.write('The user entered', fruit_choice)
 
-# API request to fruityvice.com
-fruityvice_response = re.get('https://fruityvice.com/api/fruit/' + fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-# normalise the json version of the response
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
-# output the dataframe to the screen as a table
-streamlit.dataframe(fruityvice_normalised)
+except URLError as e:
+  streamlit.error()
+  
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
